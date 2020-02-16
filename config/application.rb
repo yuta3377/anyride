df946ff3eff03d782c14a56ff9f431f9c8adcec7
清水雄大 <shimizuyuta@Yutas.local>
2020-02-17 06:46:41 +0900

    Gemfile更新,application.rbの設定

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,19 @@ module AppName
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # I18n & Timezone
+    config.i18n.available_locales = %i(ja en)
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+  
+    config.generators do |g|
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       routing_specs: false
+    end
   end
 end
