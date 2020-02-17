f37c8b33beb7506d3f8ce728a44587a4e874372d
清水雄大 <shimizuyuta@Yutas.local>
2020-02-17 21:21:58 +0900

    Userモデル追加

---
+++ app/models/user.rb
@@ -0,0 +1,43 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  address                :string(255)      not null
+#  birthday               :date             not null
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  first_name             :string(255)      not null
+#  first_name_kana        :string(255)      not null
+#  last_name              :string(255)      not null
+#  last_name_kana         :string(255)      not null
+#  nickname               :string(255)      not null
+#  phone_number           :string(255)      not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname,
+            :last_name,
+            :first_name,
+            :last_name_kana,
+            :first_name_kana,
+            :birthday,
+            :address,
+            :phone_number,
+            presence: true
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,32 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  address                :string(255)      not null
+#  birthday               :date             not null
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  first_name             :string(255)      not null
+#  first_name_kana        :string(255)      not null
+#  last_name              :string(255)      not null
+#  last_name_kana         :string(255)      not null
+#  nickname               :string(255)      not null
+#  phone_number           :string(255)      not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
+FactoryBot.define do
+  factory :user do
+    
+  end
+end
