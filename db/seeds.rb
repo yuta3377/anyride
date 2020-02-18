82d599f4b62c905ef39c3ce2f5f9b8f88af8f736
清水雄大 <shimizuyuta@Yutas.local>
2020-02-19 06:10:49 +0900

    Faker導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  Bike.create(
+    name: Faker::Bike.name,
+    price: 5000,
+    year: 2020,
+    displacement: 600,
+    mileage: 10000,
+    description: "見た目の格好良さと加速が売りです！"
+  )
+end
