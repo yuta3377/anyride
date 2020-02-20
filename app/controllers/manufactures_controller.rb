66423688a98ee4f624c24ac01d40f20a68dbfe5c
清水雄大 <shimizuyuta@Yutas.local>
2020-02-21 07:22:40 +0900

    メーカー/ボディタイプ絞り込み機能追加

--- app/controllers/manufactures_controller.rb
+++ app/controllers/manufactures_controller.rb
@@ -2,4 +2,9 @@ class ManufacturesController < ApplicationController
   def index
     @manufactures = Manufacture.all
   end
+
+  def show
+    @manufacture = Manufacture.find(params[:id])
+    @bikes_of_manufacture = @manufacture.bikes
+  end
 end
