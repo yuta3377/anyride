9eb0976c21d535d2435e913331abe5f6cf1879e8
清水雄大 <shimizuyuta@Yutas.local>
2020-03-05 14:00:50 +0900

    reservation model誤記修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -23,6 +23,6 @@
 #
 
 class Reservation < ApplicationRecord
-  belongs_to :owner, class_name: "User", foreign_key: :owner_id
+  belongs_to :borrower, class_name: "User", foreign_key: borrower_id
   belongs_to :bike
 end
